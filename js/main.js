c4ebf17cb3a2ab26da85069fa2c7e5f7a736c42e
Santiago Peñuela <ing.santiago93@gmail.com>
2015-10-23 18:16:43 -0500

    Actualización 0.1.3 Se actualizó el "look and feel" de la plantilla con nuevos componentes, imágenes, fondos y eventos, aún está propenso a cambios y falta versión responsive de la plantilla.

--- js/main.js
+++ js/main.js
@@ -24,4 +24,44 @@ $(document).ready(function()
 		$(this).siblings('span').fadeOut("fast");
 	});
 
+	$("#unit_span").click(function()
+		{
+			if($("#extended_descrip_leccion").is(":visible"))
+			{
+				$("#extended_descrip_leccion").slideUp("fast");
+				$("#extended_descrip_leccion").removeClass("visible_me");
+			}
+
+			if($("#extended_descrip_unidad").hasClass("visible_me"))
+			{
+				$("#extended_descrip_unidad").slideUp("slow");
+				$("#extended_descrip_unidad").removeClass("visible_me");
+			}
+			else
+			{
+				$("#extended_descrip_unidad").slideDown("slow");
+				$("#extended_descrip_unidad").addClass("visible_me");
+			}			
+		});
+
+	$("#lesson_span").click(function()
+		{
+			if($("#extended_descrip_unidad").is(":visible"))
+			{
+				$("#extended_descrip_unidad").slideUp("fast");
+				$("#extended_descrip_unidad").removeClass("visible_me");
+			}
+
+			if($("#extended_descrip_leccion").hasClass("visible_me"))
+			{
+				$("#extended_descrip_leccion").slideUp("slow");
+				$("#extended_descrip_leccion").removeClass("visible_me");
+			}
+			else
+			{
+				$("#extended_descrip_leccion").slideDown("slow");
+				$("#extended_descrip_leccion").addClass("visible_me");
+			}
+		});
+
 });
