4e814fe2c7b443487594f7f492b1e86942bf6cdc
Heiner Angarita <heinerangarita@gmail.com>
2015-10-26 16:26:31 -0500

    Estructura inicial

--- application/controller/system.js
+++ application/controller/system.js
@@ -5,3 +5,25 @@
  */
 
 
+(function () {
+    var app = angular.module("plantilla-transferencia", ["routes"]);
+
+    app.controller("CommentExample", ["$http", function ($http) {
+            this.comments = [];
+            this.postComment = {};
+
+            var comments = this;
+
+            $http.get("config.json").success(function (data) {
+                comments.comments = data;
+            });
+
+            this.setComment = function () {
+                console.log(this.postComment);
+                this.comments.push(this.postComment);
+                this.postComment = {};
+            };
+
+        }]);
+
+})();
