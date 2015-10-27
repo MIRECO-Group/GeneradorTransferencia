e0aa9dfd7ee2ee22c2f52f4bf67d8c2e15ce3481
Heiner Angarita <heinerangarita@gmail.com>
2015-10-27 15:26:05 -0500

    Funcionalidad Angular basica

--- application/controller/system.js
+++ application/controller/system.js
@@ -12,13 +12,38 @@ var PLANTILLA = {};
 (function () {
     var app = angular.module("plantilla-transferencia", ["routes"]);
 
-    app.controller("Initialize", ["$http", "$log", function ($http, $log) {
+    app.controller("Initialize", ["$http", "$log", "$scope", function ($http, $log, $scope) {
             var plantilla = this;
             plantilla.config = {};
 
             $http.get("application/system/config.json").success(function (data) {
                 plantilla.config = data;
                 PLANTILLA = data;
+                $scope.$emit('plantillaLoaded', {data: data});
+                $log.log(data);
             });
+        }]);
+
+    app.controller("Paginacion", ["$log", "$http", "$scope", function ($log, $http, $scope) {
+            //var paginacion = this;
+            this.paginas = {};
+            this.actual = 0;
+            this.final = 1;
+
+            var that = this;
+
+            //$scope.$on('plantillaLoaded', function (event, data) {
+            $http.get("application/system/config.json").success(function (data) {
+                that.paginas = data.macrorecursos;
+                that.actual = 1;
+                $.each(that.paginas, function (k, v) {
+
+                });
+            });
+
+            /*$http.get("application/system/config.json").success(function (data) {
+             
+             });*/
+
         }]);
 })();
